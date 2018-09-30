eb14a10b09afe6a25fb826cfc16597ee93514a45
Mollie <molliechindavong@gmail.com>
2018-09-30 02:34:29 -0400

    updating all files

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
-$('body').scrollspy({ target: '#navbar' })
+$('body').scrollspy({target: ".navbar"})
 
-$('[data-spy="scroll"]').on('activate.bs.scrollspy', function () {
-    // do something…
-  })
+$(document).ready(function(){
+    $(window).scroll(function () {
+           if ($(this).scrollTop() > 50) {
+               $('#back-to-top').fadeIn();
+           } else {
+               $('#back-to-top').fadeOut();
+           }
+       });
+       // scroll body to 0px on click
+       $('#back-to-top').click(function () {
+           $('#back-to-top').tooltip('hide');
+           $('body,html').animate({
+               scrollTop: 0
+           }, 800);
+           return false;
+       });
+       
+       $('#back-to-top').tooltip('show');
+
+});
