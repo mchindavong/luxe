b3171a06caf4c70770fb96c3dabc34e92701fc90
Mollie <molliechindavong@gmail.com>
2018-10-02 02:54:16 -0400

    updating files

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ $(document).ready(function(){
     $('body').scrollspy({target: ".navbar", offset: 50});   
 });
 
+// refresh to top
+// $(document).ready(function(){
+//     $('html, body').scrollTop(0);
+
+//     $(window).on('load', function() {
+//     setTimeout(function(){
+//         $('html, body').scrollTop(0);
+//     }, 0);
+//  });
+// });
+
 $(document).ready(function(){
     $(window).scroll(function () {
            if ($(this).scrollTop() > 50) {
